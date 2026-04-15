1ed46d84b406b9c2ce657e4c8d9718b4882fb38b
0p4n1k <0p4n1k@mailfence.com>
2026-04-15 21:22:09 +0200

    feat: add IMMUTABLE_TYPES and filter to shared utils

--- noctyra/utils/__init__.py
+++ noctyra/utils/__init__.py
@@ -1,4 +1,13 @@
 from .logger import LOGGER
-from .Shared import BIN_OPS, UNARY_OPS, COMP_OPS, ENCODING, FUNCS, ATTR
+from .Shared import BIN_OPS, UNARY_OPS, COMP_OPS, ENCODING, FUNCS, ATTR, IMMUTABLE_TYPES
 
-__all__ = ["LOGGER", "BIN_OPS", "UNARY_OPS", "COMP_OPS", "ENCODING", "FUNCS", "ATTR"]
+__all__ = [
+    "LOGGER",
+    "BIN_OPS",
+    "UNARY_OPS",
+    "COMP_OPS",
+    "ENCODING",
+    "FUNCS",
+    "ATTR",
+    "IMMUTABLE_TYPES",
+]
