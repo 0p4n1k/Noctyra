f8d5f4e4ca2a67fd08c723d1a2436ac7a756f3be
0p4n1k <0p4n1k@mailfence.com>
2026-04-15 21:21:40 +0200

    feat: add deepcopy to CustomFunction for safer evaluation

--- noctyra/core/Function.py
+++ noctyra/core/Function.py
@@ -1,7 +1,14 @@
 import ast
+import copy
 
 
 class CustomFunction:
     def __init__(self, body: ast.AST, args: ast.arguments) -> None:
-        self.body = body
-        self.args = args
+        self.body = copy.deepcopy(body)
+        self.args = copy.deepcopy(args)
+
+    def get_body(self):
+        return copy.deepcopy(self.body)
+
+    def get_args(self):
+        return copy.deepcopy(self.args)
